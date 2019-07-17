1ae9e52e175cb0cba3ba1e3dbdb904d9fed5a309
royslg <ngxiaoyi@gmail.com>
2019-07-18 02:08:23 +0800

    可行性

--- html2wxml-component/html2wxml.js
+++ html2wxml-component/html2wxml.js
@@ -1,3 +1,20 @@
+// rich-text 官方富文本组件，缺点
+// 一些常用标签不支持，如section
+// 不能实现图片和链接的点击
+// 不支持音视频
+// 不支持解析style标签
+// 
+// 转换官方组件不支持的标签
+// //以u标签为例
+// case 'u':
+//     name = 'span';
+//     attrs.style = 'text-decoration:underline;' + attrs.style;
+//     break;
+// 
+// 顶层标签上加上user-select:text;-webkit-user-select 解决文本复制
+// 
+// 嵌套循环问题： 组件来解决层级深的问题
+
 let html2wxml = require('html2wxml-main.js');
 
 Component({
